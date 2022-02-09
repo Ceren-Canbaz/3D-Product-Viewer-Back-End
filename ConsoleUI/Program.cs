9d6e266dba505035ef40d9ee06e2c3b4676ff2f8
Ceren <ceren_canbaz@hotmail.com>
2022-02-09 16:52:08 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{ 
+			//test ortamı
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+	public class Product:IEntity
+	{
+
+		//classın defaulttaki hali internaldır. bu sadece o projenin içindeki üyelerin classa ulasabilmesini sağlar.
+		//classı public yaptık ->bu classa diğer katmanlar da ulaşabilsin anlamına gelmektedir.
+
+		public int ProductId { get; set; }
+		public int CategoryId { get; set; }
+		public string ProductName { get; set; }
+		public short UnitsInStock { get; set; }
+		public decimal UnitPrice { get; set; }
+
+	}
+}
