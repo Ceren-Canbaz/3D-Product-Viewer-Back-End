ca6b2a42b15013e77ce73e59a7971f4a8261cfb1
Ceren <ceren_canbaz@hotmail.com>
2022-02-14 21:14:58 +0300

    14.02.2022

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -16,10 +16,24 @@ namespace Business.Concrete
 			_productDal = productDal;
 		}
 
+		public Product Get(int id)
+		{
+			return _productDal.Get(p => p.ProductId==id);
+		}
+
 		public List<Product> GetAll()
 		{
-			//iş kodları 
 			return _productDal.GetAll();
+		}
+
+		public List<Product> GetAllByCategory(int id)
+		{
+			return _productDal.GetAll(p=>p.CategoryId==id);
+		}
+
+		public List<Product> GetByUnitPrice(decimal min, decimal max)
+		{
+			return _productDal.GetAll(p => p.UnitPrice>=min && p.UnitPrice<=max);
 		}
 	}
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,10 +11,14 @@ namespace ConsoleUI
 		{
 			//test ortamı
 			ProductManager produtcManager = new ProductManager(new EfProductDal());
-			foreach (var product in produtcManager.GetAll())
+			foreach (var product in produtcManager.GetAllByCategory(2))
 			{
 				Console.WriteLine(product.ProductName);
 			}
+			ProductManager productManager2 = new ProductManager(new EfProductDal());
+			string name=productManager2.Get(2).ProductName;
+			Console.WriteLine("idsi 2 olan ürünün ismi: "+name);
+			
 		}
 	}
 }
