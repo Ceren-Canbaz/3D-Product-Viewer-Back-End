ce72295d010a188f6b174d81fcfa7d218fc94102
Ceren <ceren_canbaz@hotmail.com>
2022-02-14 19:33:55 +0300

    14.02.2022

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-	public interface IEntityRepository<T>
+	//Generic constaint
+	//class:referans tip olabilir
+	//IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+	//new(): new'lenebilir olmalı. (interface newlenemez böylece interfacein yazılabileceği durumlara kısıt koyduk.)
+	public interface IEntityRepository<T> where T:class,IEntity,new()
 	{
+		
 		List<T> GetAll(Expression<Func<T,bool>>filter=null);
 		T Get(Expression<Func<T, bool>> filter);
 		void Add(T entity);
