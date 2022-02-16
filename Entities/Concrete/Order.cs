1d48f26a517e215b282c2b0c064263d8dddcd008
Ceren <ceren_canbaz@hotmail.com>
2022-02-16 23:17:22 +0300

    16.02.2022

---
+++ Entities/Concrete/Order.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+	//veritabanı nersnesi oldugu icin IEntityi implemente ediyoruz
+	public class Order:IEntity
+	{
+		//veritabanindaki tip ve isimle birebir ayni olmali.
+		public int OrderId { get; set; }
+		public string CustomerId { get; set; }
+		public int EmployeeId { get; set; }
+		public DateTime OrderDate { get; set; }
+		public string ShipCity { get; set; }
+	}
+}
